ef1d41b0491f96fd794f7d029d887c03bd1a1b95
Brent Knudsen <>
2021-09-20 07:47:23 -0600

    fixed merge conflict in material.module.ts file (3 changes)

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -6,12 +6,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-<<<<<<< HEAD
 import { MatSelectModule } from '@angular/material/select';
 import {MatToolbarModule} from '@angular/material/toolbar';
-=======
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
->>>>>>> 01b76497380b124bfd392e1180f2a91d6f6a5b6e
 
 
 @NgModule({
@@ -23,12 +20,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatFormFieldModule,
     MatButtonModule,
     MatIconModule,
-<<<<<<< HEAD
     MatSelectModule,
     MatToolbarModule,
-=======
     MatProgressSpinnerModule,
->>>>>>> 01b76497380b124bfd392e1180f2a91d6f6a5b6e
   ],
   exports: [
     MatSidenavModule,
@@ -36,12 +30,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatFormFieldModule,
     MatButtonModule,
     MatIconModule,
-<<<<<<< HEAD
     MatSelectModule,
     MatToolbarModule,
-=======
     MatProgressSpinnerModule,
->>>>>>> 01b76497380b124bfd392e1180f2a91d6f6a5b6e
   ],
 })
 export class MaterialModule { }
