46e94d17627205aca1b25f3794ac0a27d87b95e7
ParkerGates <PARKER.GATES1052@STU.MTEC.EDU>
2021-09-23 09:46:36 -0600

    dumby data set added

--- src/app/components/top-headlines/top-headlines.component.ts
+++ src/app/components/top-headlines/top-headlines.component.ts
@@ -10,19 +10,7 @@ import { HttpClient } from "@angular/common/http"
 })
 export class TopHeadlinesComponent implements OnInit {
   apiPayload: any = "loading";
-  dumby = {
-    source: {
-      id: "ars-technica",
-      name: "Ars Technica"
-      },
-      author: "Jonathan M. Gitlin",
-      title: "House infrastructure bill includes new tax credits for new and used EVs",
-      description: "There are price and earnings caps on the tax credits, plus made-in-USA requirements.",
-      url: "https://arstechnica.com/cars/2021/09/house-infrastructure-bill-includes-new-tax-credits-for-new-and-used-evs/",
-      urlToImage: "https://cdn.arstechnica.net/wp-content/uploads/2021/09/GettyImages-1201454382-760x380.jpg",
-      publishedAt: "2021-09-13T15:02:07Z",
-      content: "Enlarge/ The House Ways and Means Committee has proposed a wide-ranging overhaul of the current electric vehicle incentives as part of the infrastructure bill. \r\n12 with 11 posters participating, inc… [+6370 chars]"
-  }
+  dumby = this.api.dumby;
 
   constructor(
     private api: NewsApiService,
@@ -36,6 +24,7 @@ export class TopHeadlinesComponent implements OnInit {
       const link = this.api.topHeadlinesEndpoint(category);
       // this.http.get(link).subscribe(data => {
       //   this.apiPayload = data;
+      //   console.log(data);
       // });
       this.apiPayload = link;
     });
