8360721a5110bc7f5055c8964a253ed86c81456e
Parker Gates <parkergates14@gmail.com>
2021-09-23 21:54:12 -0600

    responsive sidenav fix , aritcle scroll response set, minor formatting changes

--- src/app/services/news-api.service.ts
+++ src/app/services/news-api.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -8,18 +7,16 @@ export class NewsApiService {
   apiKey: string = "75e20b21ea8f499190960a2dfbc5f410";
   categories = ['business','entertainment','general','health','science','sports','technology'];
 
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor() { }
 
   topHeadlinesEndpoint(category: any) {
-    let link = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${this.apiKey}`;
+    let link = `https://newsapi.org/v2/top-headlines?category=${category}&country=us&apiKey=${this.apiKey}`;
     return link;
   }
 
   everythingEndpoint(query: string, category: any) {
     if (category === "general") {
-      let link = `https://newsapi.org/v2/everything?q=${query}&apiKey=${this.apiKey}`;
+      let link = `https://newsapi.org/v2/everything?q=${query}&language=en&apiKey=${this.apiKey}`;
       return link;
     }
     else if (this.categories.includes(category)) {
